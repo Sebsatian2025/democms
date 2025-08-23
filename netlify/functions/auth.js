c989ea55b2a0f81726aa5def81fb14f12ed8b41a
kitdelanzamientodigital <sebastian_6435@hotmail.com>
2025-08-23 19:04:04 +0200

    Delete netlify/functions/auth.js

--- netlify/functions/auth.js
+++
@@ -1,8 +0,0 @@
-// netlify/functions/auth.js
-const { handler } = require('@sveltia/auth/netlify')
-
-exports.handler = handler({
-  clientId: process.env.SVELTIA_CLIENT_ID,
-  clientSecret: process.env.SVELTIA_CLIENT_SECRET,
-  hostname: 'sebastiandemo.netlify.app',  // o tu dominio netlify.app
-})
