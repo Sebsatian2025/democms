c51d3a329aac3a813de66c8b4a0dc1f1d81e9270
kitdelanzamientodigital <sebastian_6435@hotmail.com>
2025-08-24 00:00:37 +0200

    Update auth.js

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -7,26 +7,27 @@ const { OAUTH_PROVIDER = 'github' } = process.env;
 const oauth = new OAuth(OAUTH_PROVIDER);
 
 export const handler = async (event) => {
-  const { provider, scope, referer } = event.queryStringParameters;
+  const { provider = 'github', scope = 'repo,user', referer = '/' } = event.queryStringParameters;
+
+  // 👇 ahora se genera la URL con redirect_uri dentro de OAuth
   const authorizationURL = oauth.getAuthorizationURL(scope);
+
   const providerCookie = cookie.serialize('provider', provider, {
     httpOnly: true,
     path: '/.netlify/functions/callback',
     maxAge: 3600,
   });
+
   const refererCookie = cookie.serialize('referer', referer, {
     httpOnly: true,
     path: '/.netlify/functions/callback',
     maxAge: 3600,
   });
 
-  // Combina los encabezados Set-Cookie en una sola cadena
-  const cookieHeader = `${providerCookie}, ${refererCookie}`;
-
   return {
     statusCode: 302,
     headers: {
-      'Set-Cookie': cookieHeader, // Ahora es una sola cadena
+      'Set-Cookie': [providerCookie, refererCookie], // 👈 mejor como array
       Location: authorizationURL,
       'Cache-Control': 'no-cache',
     },
