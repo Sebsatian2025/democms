6debf0bef32f3f11f9c3acffdb8d79aee14e547b
kitdelanzamientodigital <sebastian_6435@hotmail.com>
2025-08-24 18:55:42 +0200

    Update oauth.js

--- netlify/functions/common/oauth.js
+++ netlify/functions/common/oauth.js
@@ -1,7 +1,11 @@
 // netlify/functions/common/oauth.js
-import { AuthorizationCode } from 'simple-oauth2';
+const { AuthorizationCode } = require('simple-oauth2')
 
-const { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, OAUTH_CALLBACK } = process.env;
+const {
+  GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET,
+  OAUTH_CALLBACK,
+} = process.env
 
 const OAUTH_PROVIDERS = {
   github: {
@@ -9,42 +13,42 @@ const OAUTH_PROVIDERS = {
     client_secret: GITHUB_CLIENT_SECRET,
     url: 'https://github.com',
   },
-};
+}
 
-export class OAuth {
+class OAuth {
   constructor(provider) {
-    this.provider = provider;
-    const {
-      [provider]: { client_id, client_secret, url },
-    } = OAUTH_PROVIDERS;
+    this.provider = provider
+    const cfg = OAUTH_PROVIDERS[provider] || {}
+    const { client_id, client_secret, url } = cfg
 
     if (!client_id || !client_secret) {
-      throw new Error(`Missing OAuth config for provider ${provider}`);
+      throw new Error(`Missing OAuth config for provider ${provider}`)
     }
 
-    const config = {
+    this.client = new AuthorizationCode({
       client: { id: client_id, secret: client_secret },
       auth: {
         tokenHost: url,
         tokenPath: '/login/oauth/access_token',
         authorizePath: '/login/oauth/authorize',
       },
-    };
-
-    this.client = new AuthorizationCode(config);
+    })
   }
 
   getAuthorizationURL(scope) {
     return this.client.authorizeURL({
       redirect_uri: OAUTH_CALLBACK,
       scope,
-    });
+    })
   }
 
   async getToken(code) {
     return this.client.getToken({
       code,
       redirect_uri: OAUTH_CALLBACK,
-    });
+    })
   }
 }
+
+module.exports = { OAuth }
+
