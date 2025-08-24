2f8ca921119a6ab83115742cf91ed717f0a55543
kitdelanzamientodigital <sebastian_6435@hotmail.com>
2025-08-24 18:21:07 +0200

    Update auth.js

--- netlify/functions/auth.js
+++ netlify/functions/auth.js
@@ -7,27 +7,17 @@ const oauth = new OAuth(OAUTH_PROVIDER);
 
 export const handler = async (event) => {
   const { referer = '/' } = event.queryStringParameters;
-
-  // Definimos los permisos que necesitamos
-  const scope = 'repo read:user';
-
-  // Generamos la URL de autorización de GitHub
+  const scope = 'public_repo read:user';
   const authorizationURL = oauth.getAuthorizationURL(scope);
 
-  // Guardamos la cookie con el provider correcto
   const providerCookie = cookie.serialize('provider', OAUTH_PROVIDER, {
     httpOnly: true,
     path: '/.netlify/functions/callback',
-    maxAge: 3600, // 1 hora
+    maxAge: 3600,
   });
 
   return {
     statusCode: 302,
-    headers: {
-      'Set-Cookie': providerCookie,
-      Location: authorizationURL,
-      'Cache-Control': 'no-cache',
-    },
+    headers: { 'Set-Cookie': providerCookie, Location: authorizationURL, 'Cache-Control': 'no-cache' },
   };
 };
-
