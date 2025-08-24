414140f297ea7868b2cc1f25790215ee8d3d4c5e
kitdelanzamientodigital <sebastian_6435@hotmail.com>
2025-08-24 19:00:33 +0200

    Update oauth.js

--- netlify/functions/common/oauth.js
+++ netlify/functions/common/oauth.js
@@ -1,53 +1,48 @@
 // netlify/functions/common/oauth.js
-const { AuthorizationCode } = require('simple-oauth2')
-
-const {
-  GITHUB_CLIENT_ID,
-  GITHUB_CLIENT_SECRET,
-  OAUTH_CALLBACK,
-} = process.env
+const { AuthorizationCode } = require('simple-oauth2');
 
 const OAUTH_PROVIDERS = {
   github: {
-    client_id: GITHUB_CLIENT_ID,
-    client_secret: GITHUB_CLIENT_SECRET,
-    url: 'https://github.com',
-  },
-}
+    client_id:     process.env.GITHUB_CLIENT_ID,
+    client_secret: process.env.GITHUB_CLIENT_SECRET,
+    url:           'https://github.com'
+  }
+};
 
 class OAuth {
   constructor(provider) {
-    this.provider = provider
-    const cfg = OAUTH_PROVIDERS[provider] || {}
-    const { client_id, client_secret, url } = cfg
-
-    if (!client_id || !client_secret) {
-      throw new Error(`Missing OAuth config for provider ${provider}`)
+    const cfg = OAUTH_PROVIDERS[provider] || {};
+    if (!cfg.client_id || !cfg.client_secret) {
+      throw new Error(`Missing OAuth config for provider ${provider}`);
     }
-
+    this.provider = provider;
     this.client = new AuthorizationCode({
-      client: { id: client_id, secret: client_secret },
+      client: { id: cfg.client_id, secret: cfg.client_secret },
       auth: {
-        tokenHost: url,
-        tokenPath: '/login/oauth/access_token',
-        authorizePath: '/login/oauth/authorize',
-      },
-    })
+        tokenHost:   cfg.url,
+        tokenPath:   '/login/oauth/access_token',
+        authorizePath: '/login/oauth/authorize'
+      }
+    });
   }
 
   getAuthorizationURL(scope) {
     return this.client.authorizeURL({
-      redirect_uri: OAUTH_CALLBACK,
-      scope,
-    })
+      redirect_uri: process.env.OAUTH_CALLBACK,
+      scope
+    });
   }
 
   async getToken(code) {
     return this.client.getToken({
       code,
-      redirect_uri: OAUTH_CALLBACK,
-    })
+      redirect_uri: process.env.OAUTH_CALLBACK
+    });
   }
+}
+
+module.exports = { OAuth };
+
 }
 
 module.exports = { OAuth }
