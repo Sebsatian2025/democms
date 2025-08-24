82c4a2115cc3dfc25aea4364fda21da7dcd4336a
kitdelanzamientodigital <sebastian_6435@hotmail.com>
2025-08-24 18:20:49 +0200

    Update callback.js

--- netlify/functions/callback.js
+++ netlify/functions/callback.js
@@ -4,25 +4,17 @@ import { OAuth } from './common/oauth.js';
 
 export const handler = async (event) => {
   const { code } = event.queryStringParameters;
-
-  // Leemos la cookie del provider
   const { provider } = cookie.parse(event.headers.cookie || '');
   if (!provider) {
-    return {
-      statusCode: 400,
-      body: 'Provider not found in cookie',
-    };
+    return { statusCode: 400, body: 'Provider not found in cookie' };
   }
 
   const oauth = new OAuth(provider);
 
   try {
-    // Obtenemos el token de GitHub usando el código recibido
     const { token } = await oauth.getToken(code);
     const { access_token, token_type } = token;
 
-    // Redirigimos al admin usando un pequeño HTML que asegura que Sveltia
-    // lea el hash fragment correctamente y evite el bucle infinito
     return {
       statusCode: 200,
       headers: { 'Content-Type': 'text/html' },
@@ -32,7 +24,6 @@ export const handler = async (event) => {
             <script>
               const token = "${access_token}";
               const type = "${token_type}";
-              // Redirigimos al admin con el hash fragment
               window.location.href = "/admin/#access_token=" + token + "&token_type=" + type;
             </script>
           </body>
@@ -41,10 +32,8 @@ export const handler = async (event) => {
     };
   } catch (e) {
     console.error(e);
-    return {
-      statusCode: 500,
-      body: 'Server Error',
-    };
+    return { statusCode: 500, body: 'Server Error' };
   }
 };
+
 
